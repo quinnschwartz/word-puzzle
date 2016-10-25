48026a362a491fe04436fb6f415c8dc3a28676ba
James Lee and Quinn Schwartz <quinn@>
2016-10-25 10:49:40 -0700

    add for loop to complete the js code.

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,23 @@ $(document).ready(function() {
 
      var sentence = $("input#1").val();
      var vowels = ["a", "e", "i", "o", "u"];
+     var sentenceSplit = sentence.split("");
+     var length = sentenceSplit.length
 
+     var finalArray = [];
+     for(var index = 0; index < length; index += 1) {
+       var letter = sentenceSplit.shift();
+       if (letter === "a" || letter === "e" || letter === "i" || letter === "o" || letter === "u") {
+         finalArray.push("-");
+       } else {
+         finalArray.push(letter);
+       }
+      //return sentenceSplit.replace(vowels, "-");
+     }
+     var finalSentence = finalArray.join("");
+
+     $("#puzzle").show();
+     $("#output").text(finalSentence);
 
   });
+});
