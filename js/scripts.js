d4bbc2e265867532bf64b62c0565fb6c16f3930e
James Lee and Quinn Schwartz <james@>
2016-10-25 09:19:27 -0700

    create files and write basic html and js code.

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $("#blanks form").submit(function(event) {
+     event.preventDefault();
+
+     var sentence = $("input#1").val();
+     var vowels = ["a", "e", "i", "o", "u"];
+
+
+  });
